d9cc9d11eb99d08536e83615bdaf33d414587a08
Nans Pellicari <nans.pellicari@gmail.com>
2020-10-29 12:55:15 +0100

    step 4: children's style customizations

--- Source/MyGameEditor/Private/Customization/MyStructCustomization.cpp
+++ Source/MyGameEditor/Private/Customization/MyStructCustomization.cpp
@@ -1,5 +1,6 @@
 #include "Customization/MyStructCustomization.h"
 
+#include "IDetailChildrenBuilder.h"
 #include "SlateBasics.h"
 #include "MyGame/MyStruct.h"
 #include "PropertyEditor/Public/DetailLayoutBuilder.h"
@@ -52,7 +53,113 @@ void FMyStructCustomization::CustomizeHeader(TSharedRef<IPropertyHandle> StructP
 
 void FMyStructCustomization::CustomizeChildren(TSharedRef<IPropertyHandle> StructPropertyHandle,
 	class IDetailChildrenBuilder& StructBuilder,
-	IPropertyTypeCustomizationUtils& StructCustomizationUtils) {}
+	IPropertyTypeCustomizationUtils& StructCustomizationUtils)
+{
+	// First we need to retrieve every Property handles
+	const TSharedPtr<IPropertyHandle> AmountPropertyHandle =
+		StructPropertyHandle->GetChildHandle(GET_MEMBER_NAME_CHECKED(FMyStruct, Amount));
+	const TSharedPtr<IPropertyHandle> LhRangePropertyHandle =
+		StructPropertyHandle->GetChildHandle(GET_MEMBER_NAME_CHECKED(FMyStruct, RangeMin));
+	const TSharedPtr<IPropertyHandle> RhRangePropertyHandle =
+		StructPropertyHandle->GetChildHandle(GET_MEMBER_NAME_CHECKED(FMyStruct, RangeMax));
+	const TSharedPtr<IPropertyHandle> TypePropertyHandle = StructPropertyHandle->GetChildHandle(
+		GET_MEMBER_NAME_CHECKED(FMyStruct, Type)
+	);
+
+	// and check them before using them
+	check(
+		AmountPropertyHandle.IsValid()
+		&& LhRangePropertyHandle.IsValid()
+		&& RhRangePropertyHandle.IsValid()
+		&& TypePropertyHandle.IsValid()
+	);
+
+	// clang-format off
+	StructBuilder.AddCustomRow(LOCTEXT("MyStructRow", "MyStruct"))
+	[
+		SNew(SBorder)
+		.BorderImage(FEditorStyle::GetBrush("ToolPanel.GroupBorder"))
+		.BorderBackgroundColor(FLinearColor(255.f, 152.f, 0))
+		.Content()
+		[
+			SNew(SWrapBox)
+			.UseAllottedWidth(true)
+			+ SWrapBox::Slot()
+			.Padding(5.f, 0.f)
+			[
+				SNew(SVerticalBox)
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					TypePropertyHandle->CreatePropertyNameWidget()
+				]
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					TypePropertyHandle->CreatePropertyValueWidget()
+				]
+			]
+			+ SWrapBox::Slot()
+			.Padding(5.f, 0.f)
+			[
+				SNew(SBox)
+				.MinDesiredWidth(70.f)
+				[
+					SNew(SVerticalBox)
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						AmountPropertyHandle->CreatePropertyNameWidget()
+					]
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						AmountPropertyHandle->CreatePropertyValueWidget()
+					]
+				]
+			]
+			+ SWrapBox::Slot()
+			.Padding(5.f, 0.f)
+			[
+				SNew(SBox)
+				.MinDesiredWidth(70.f)
+				[
+					SNew(SVerticalBox)
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						LhRangePropertyHandle->CreatePropertyNameWidget()
+					]
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						LhRangePropertyHandle->CreatePropertyValueWidget()
+					]
+				]
+			]
+			+ SWrapBox::Slot()
+			.Padding(5.f, 0.f)
+			[
+				SNew(SBox)
+				.MinDesiredWidth(70.f)
+				[
+					SNew(SVerticalBox)
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						RhRangePropertyHandle->CreatePropertyNameWidget()
+					]
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					[
+						RhRangePropertyHandle->CreatePropertyValueWidget()
+					]
+				]
+			]
+		]
+	];
+	// clang-format on
+}
 
 void FMyStructCustomization::OnTypeChanged(TSharedPtr<IPropertyHandle> TypePropertyHandle)
 {
